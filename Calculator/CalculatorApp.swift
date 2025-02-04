b99aead91ed40a0be1063e160a2b5116a26cca44
CharlesMich <charleedongo@hotmail.com>
2025-02-03 22:14:48 -0600

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Charles Michael on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
